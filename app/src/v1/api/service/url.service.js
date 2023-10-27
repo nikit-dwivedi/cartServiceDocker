bb91c1a05a16118f2bf6b47be81e6a82bf4f5376
nikitdwivedi <nikitdwivedi1997@gmail.com>
2023-10-28 01:19:12 +0530

    server ip change

--- app/src/v1/api/service/url.service.js
+++ app/src/v1/api/service/url.service.js
@@ -1,7 +1,7 @@
 const inventoryBaseUrl = 'http://139.59.60.119:9000'
 const inventoryBaseUrlProd =  'https://inventory.fablocdn.com'
 const inventoryBaseUrlLocal =  'http://localhost:9000'
-const inventoryBaseUrlServer =  'http://172.17.0.1:8084'
+const inventoryBaseUrlServer =  'http://172.22.0.3:8080'
 
 exports.getProductDetailUrl = () => {
     return `${inventoryBaseUrlServer}/v1/menu/customization/reverse`
